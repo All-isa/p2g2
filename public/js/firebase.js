6dadbffb66caff06091cc41fccd657a14767b476
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-05-14 20:58:46 -0500

    firebase database set up to receive images

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,4 +1,5 @@
-var config = {
+$(document).ready(function () {
+  var config = {
     apiKey: "AIzaSyAn3Z01eZ7KULCJwK73Ku7xafnDJQ78Kow",
     authDomain: "artists-photo.firebaseapp.com",
     databaseURL: "https://artists-photo.firebaseio.com",
@@ -7,3 +8,86 @@ var config = {
     messagingSenderId: "1076918173975"
   };
   firebase.initializeApp(config);
+  console.log("firebase");
+
+  var storage = firebase.storage();
+
+  var imageUpload = $("#inputGroupFile04");
+
+  console.log($("#inputGroupFile04"));
+  $("#inputGroupFile04").change(function (event) {
+    console.log("hi dan!");
+    $("#tempFile").text(event.target.files[0].name);
+    console.log(event.target.files[0].name);
+  });
+console.log($("#inputGroupFile04"));
+
+var directory = "";
+  imageUpload.change(function (event) {
+    console.log(event);
+    var file = event.target.files[0];
+
+    directory = artist.email + "/" + file.name;
+    var storageRef = firebase.storage().ref(directory);
+
+    var task = storageRef.put(file);
+  });
+
+  console.log("/" + directory);
+  // var starsRef = storage.ref(directory);
+
+  // var image_url = "";
+
+  // starsRef.getDownloadURL().then(function(url) {
+  //   image_url = url;
+  //   console.log(url);
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
