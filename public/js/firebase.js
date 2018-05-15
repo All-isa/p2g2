9d0dcb9c0f853512adeed12e76ad19c9b115e4fa
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-05-14 21:21:56 -0500

    firebase and image urls

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -9,10 +9,17 @@ $(document).ready(function () {
   };
   firebase.initializeApp(config);
   console.log("firebase");
+  var artist = {};
+  $.get("/api/user_data").then(function(data) {
+    console.log(data);
+    artist = data;
+  });
 
   var storage = firebase.storage();
 
   var imageUpload = $("#inputGroupFile04");
+
+  console.log(artist);
 
   console.log($("#inputGroupFile04"));
   $("#inputGroupFile04").change(function (event) {
@@ -26,22 +33,25 @@ var directory = "";
   imageUpload.change(function (event) {
     console.log(event);
     var file = event.target.files[0];
-
     directory = artist.email + "/" + file.name;
     var storageRef = firebase.storage().ref(directory);
 
     var task = storageRef.put(file);
+    console.log("/" + directory);
+
+
+    var starsRef = storage.ref(directory);
+
+    var image_url = "";
+  
+    starsRef.getDownloadURL().then(function(url) {
+      image_url = url;
+      console.log(url);
+    });
   });
 
-  console.log("/" + directory);
-  // var starsRef = storage.ref(directory);
 
-  // var image_url = "";
-
-  // starsRef.getDownloadURL().then(function(url) {
-  //   image_url = url;
-  //   console.log(url);
-  // });
+ 
 
 
 
