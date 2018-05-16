222bce7c54bd33a1ed630564dfb7e82af7e3ead2
Antony Laroche <antony@larochefamily.net>
2018-05-15 23:20:29 -0500

    updated profile pic button

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -10,48 +10,116 @@ $(document).ready(function () {
   firebase.initializeApp(config);
   console.log("firebase");
   var artist = {};
-  $.get("/api/user_data").then(function(data) {
+  $.get("/api/user_data").then(function (data) {
     console.log(data);
     artist = data;
   });
-
-  var storage = firebase.storage();
-
-  var imageUpload = $("#inputGroupFile04");
-
   console.log(artist);
 
-  console.log($("#inputGroupFile04"));
-  $("#inputGroupFile04").change(function (event) {
-    console.log("hi dan!");
-    $("#tempFile").text(event.target.files[0].name);
+  var storage = firebase.storage();
+  var directory = "";
+
+
+  //uPload profile pic
+  var profilePicUpload = $("#inputGroupFile02");
+
+  $("#inputGroupFile02").change(function (event) {
+    $("#profilePicTemp").text(event.target.files[0].name);
     console.log(event.target.files[0].name);
   });
-console.log($("#inputGroupFile04"));
 
-var directory = "";
-  imageUpload.change(function (event) {
+  profilePicUpload.change(function (event) {
     console.log(event);
     var file = event.target.files[0];
-    directory = artist.email + "/" + file.name;
+    directory = artist.email + "/profile/" + file.name;
     var storageRef = firebase.storage().ref(directory);
 
     var task = storageRef.put(file);
     console.log("/" + directory);
 
+    //Monitor progress of upload
+    task.on("state_changed",
 
-    var starsRef = storage.ref(directory);
+      function progress(snapshot) {
+        // var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // uploader.value = percentage;
+      },
+      function error(err) {
+        console.log(err);
+      },
+      function complete() {
+        console.log("Upload Complete");
+        $("#inputGroupFile02").val("");
+        $("#profilePicTemp").text("Choose File");
+      
+        //Get url of image to submit to mysql
+        var starsRef = storage.ref(directory);
 
     var image_url = "";
-  
-    starsRef.getDownloadURL().then(function(url) {
+
+    starsRef.getDownloadURL().then(function (url) {
       image_url = url;
       console.log(url);
+      //enter .post request here to send url to backend
+
     });
-  });
+  }
+  );
+
+});
 
 
- 
+//Upload pictures on Portfolio tab
+
+var imageUpload = $("#inputGroupFile04");
+
+$("#inputGroupFile04").change(function (event) {
+  $("#tempFile").text(event.target.files[0].name);
+  console.log(event.target.files[0].name);
+});
+
+
+imageUpload.change(function (event) {
+  console.log(event);
+  var file = event.target.files[0];
+  directory = artist.email + "/" + file.name;
+  var storageRef = firebase.storage().ref(directory);
+
+  var task = storageRef.put(file);
+  console.log("/" + directory);
+
+  //Monitor progress of upload
+  task.on("state_changed",
+
+    function progress(snapshot) {
+      // var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      // uploader.value = percentage;
+    },
+    function error(err) {
+      console.log(err);
+    },
+    function complete() {
+      console.log("Upload Complete");
+      $("#inputGroupFile04").val("");
+      $("#tempFile").text("Choose File");
+
+      //Get url of image to submit to mysql
+      var starsRef = storage.ref(directory);
+
+      var image_url = "";
+
+      starsRef.getDownloadURL().then(function (url) {
+        image_url = url;
+        console.log(url);
+        //enter .post request here to send url to backend
+      });
+    }
+  );
+
+});
+
+
+
 
 
 
